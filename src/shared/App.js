093496f51216eb942a156777b9338bbe5aaa1b97
Jayadeep KM <kmjayadeep@gmail.com>
2018-05-23 21:10:25 +0530

    added bootstrap

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,9 +1,75 @@
 import React, { Component } from "react";
-import "./App.css";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+  Row,
+  Col,
+  Jumbotron,
+  Button
+} from "reactstrap";
+
+import './bootstrap.flatly.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
-    return <div className="test">Hello World</div>;
+    return (
+      <div>
+        <Navbar color="inverse" light expand="md">
+          <NavbarBrand href="/">BookTradingClub</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/signup/">Signup</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/">
+                  Github
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        <Jumbotron>
+          <Container>
+            <Row>
+              <Col>
+                <h1>Welcome to BookTradingClub</h1>
+                <p>
+                  <Button
+                    tag="a"
+                    color="success"
+                    size="large"
+                    href="http://reactstrap.github.io"
+                    target="_blank"
+                  >
+                    View Reactstrap Docs
+                  </Button>
+                </p>
+              </Col>
+            </Row>
+          </Container>
+        </Jumbotron>
+      </div>
+    );
   }
 }
 
