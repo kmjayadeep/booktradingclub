e6918da2e6243d4da1c072b737d6efb6f3606409
Jayadeep KM <kmjayadeep@gmail.com>
2018-06-19 07:34:11 +0530

    signup ui

--- src/shared/containers/Login/index.js
+++ src/shared/containers/Login/index.js
@@ -1,7 +1,44 @@
 import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import LoginForm from "./LoginForm";
 
-export default class Login extends Component {
+const styles = theme => ({
+  root: {
+    overflow: "hidden"
+  },
+  container: {
+    flexGrow: 1
+  },
+  card: {
+    margin: 20
+  }
+});
+
+class Login extends Component {
   render() {
-    return <div>Login here</div>;
+    const {classes} = this.props;
+    return <div className={classes.root}>
+        <Grid container className={classes.container} spacing={24} justify="center">
+          <Grid item xs={12} sm={6} md={4} lg={4} className={classes.card}>
+            <Card>
+              <CardContent>
+                <Typography variant="headline" component="h2">
+                  Login
+                </Typography>
+                <Typography color="textSecondary">
+                  to Trade Books & More
+                </Typography>
+                <LoginForm />
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </div>;
   }
 }
+
+export default withStyles(styles)(Login);
