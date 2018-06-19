ad0da91dc43c59bd5cf9f9a29c6dd00e8218c7f0
Jayadeep KM <kmjayadeep@gmail.com>
2018-06-19 09:17:21 +0530

    signup ui

---
+++ src/shared/containers/Signup/SignupForm.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Icon from "@material-ui/core/Icon";
+import Button from "@material-ui/core/Button";
+
+const styles = (theme)=>({
+  signupForm: {
+    marginTop: 20,
+    marginBottom: 20
+  },
+  margin: {
+    margin: theme.spacing.unit
+  },
+  signupButton:{
+      marginTop: 30
+  }
+});
+
+class SignupForm extends Component {
+  render() {
+    const { classes } = this.props;
+    return <Grid container className={classes.signupForm} justify="center">
+        <Grid item className={classes.margin}>
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              <Icon>person</Icon>
+            </Grid>
+            <Grid item>
+              <TextField id="input-with-icon-grid" label="Name" />
+            </Grid>
+          </Grid>
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              <Icon>person</Icon>
+            </Grid>
+            <Grid item>
+              <TextField id="input-with-icon-grid" label="Username" />
+            </Grid>
+          </Grid>
+          <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              <Icon>lock</Icon>
+            </Grid>
+            <Grid item>
+              <TextField id="input-with-icon-grid" type="password" label="Password" />
+            </Grid>
+          </Grid> <Grid container spacing={8} alignItems="flex-end">
+            <Grid item>
+              <Icon>lock</Icon>
+            </Grid>
+            <Grid item>
+              <TextField id="input-with-icon-grid" type="password" label="Confirm Password" />
+            </Grid>
+          </Grid>
+          <Grid container justify="center" className={classes.signupButton} spacing={12}>
+            <Button variant="outlined" color="primary">
+              Signup
+            </Button>
+            <Button variant="outlined" color="secondary">
+              Login
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>;
+  }
+}
+
+export default withStyles(styles)(SignupForm);
--- src/shared/containers/Signup/index.js
+++ src/shared/containers/Signup/index.js
@@ -1,10 +1,51 @@
-import React , {Component} from 'react';
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import SignupForm from "./SignupForm";
 
+const styles = theme => ({
+  root: {
+    overflow: "hidden"
+  },
+  container: {
+    flexGrow: 1
+  },
+  card: {
+    margin: 20
+  }
+});
 
-export default class SignUp extends Component{
-    render(){
-        return (
-            <div>Sign up here</div>
-        );
-    }
+class Signup extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <Grid
+          container
+          className={classes.container}
+          spacing={24}
+          justify="center"
+        >
+          <Grid item xs={12} sm={6} md={4} lg={4} className={classes.card}>
+            <Card>
+              <CardContent>
+                <Typography variant="headline" component="h2">
+                  Signup
+                </Typography>
+                <Typography color="textSecondary">
+                  to Access millions of Books
+                </Typography>
+                <SignupForm />
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
+
+export default withStyles(styles)(Signup);
