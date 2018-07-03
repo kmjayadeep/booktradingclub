55c8b89baafd5ad9940e47bb46b21f60eb5bb81f
Jayadeep KM <kmjayadeep@gmail.com>
2018-07-03 08:29:38 +0530

    dynamic topbar

--- src/shared/components/TopBar.js
+++ src/shared/components/TopBar.js
@@ -7,7 +7,18 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import HomeIcon from "@material-ui/icons/Home";
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import { withStyles } from "@material-ui/core/styles";
+import { connect } from "react-redux";
+import Icon from "@material-ui/core/Icon";
+
+const mapStateToProps = state=>{
+  return {
+    isAuth: state.authUser.isAuth,
+    user: state.authUser.user
+  }
+}
 
 const styles = {
   root: {
@@ -23,8 +34,21 @@ const styles = {
 };
 
 class TopBar extends Component {
+  state={
+    anchorEl:null
+  }
+
+  handleMenuOpen = event=>{
+    this.setState({ anchorEl: event.currentTarget })
+  }
+
+  handleMenuClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
   render() {
-    const { classes } = this.props;
+    const { classes, isAuth, user } = this.props;
+    const { anchorEl } = this.state
     return (
       <div className={classes.root}>
         <AppBar position="fixed">
@@ -41,8 +65,30 @@ class TopBar extends Component {
             <Typography variant="title" color="inherit" className={classes.flex}>
               BookSharingApp
             </Typography>
-            <Button color="inherit" to="/login" component={Link}>Login</Button>
-            <Button color="inherit" to="/signup" component={Link}>Signup</Button>
+            {!isAuth && <Button color="inherit" to="/login" component={Link}>Login</Button>}
+            {!isAuth && <Button color="inherit" to="/signup" component={Link}>Signup</Button>}
+            {isAuth && 
+            <div>
+              <Button
+                color="inherit"
+                aria-owns={anchorEl ? 'simple-menu' : null}
+                aria-haspopup="true"
+                onClick={this.handleMenuOpen}
+              >
+                {user.name}
+                <Icon>person</Icon>
+              </Button>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={this.handleMenuClose}
+              >
+                <MenuItem onClick={this.handleMenuClose} to="/profile" component={Link}>Profile</MenuItem>
+                <MenuItem onClick={this.handleMenuClose} to="/logout" component={Link}>Logout</MenuItem>
+              </Menu>
+             </div>
+             }
           </Toolbar>
         </AppBar>
       </div>
@@ -50,4 +96,6 @@ class TopBar extends Component {
   }
 }
 
-export default withStyles(styles)(TopBar);
+
+const ConnectedComponent = connect(mapStateToProps)(TopBar);
+export default withStyles(styles)(ConnectedComponent);
