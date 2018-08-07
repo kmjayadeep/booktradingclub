1fa962a58944ac22cad488083ae792a8be838580
Jayadeep KM <kmjayadeep@gmail.com>
2018-08-07 11:05:38 +0530

    api call login

--- src/shared/containers/Login/index.js
+++ src/shared/containers/Login/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import { connect } from 'react-redux';
 import {
   Button,
   Form,
@@ -9,12 +11,55 @@ import {
   Container,
   CardTitle,
   CardSubtitle,
+  Alert,
+  FormFeedback,
+  FormGroup
 } from 'reactstrap';
+
+import { loginUser } from '../../redux/actions/auth';
 
 import "./Login.css";
 
+const mapStateToProps = state => {
+  return state.authUser;
+};
+
 class Login extends Component {
+  
+  state = {
+    loginError: null,
+    loading: false
+  };
+
+  handleLogin = async () => {
+    const { email, password } = this.state;
+    const credentials = { email, password };
+    this.setState({
+      loading: true
+    });
+    try {
+      await this.props.loginUser(credentials);
+    } catch (err) {
+      this.setState({
+        loginError: err.message
+      });
+    }
+    this.setState({
+      loading: false
+    });
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+    console.log(this.state)
+  };
+
   render() {
+    if (this.props.isAuth) {
+      return <Redirect to="/" />;
+    }
     return (
       <Container>
         <Card id="login-card">
@@ -22,10 +67,14 @@ class Login extends Component {
           <CardBody>
             <CardTitle>Login</CardTitle>
             <CardSubtitle>To access thousands of books around you</CardSubtitle>
+           
             <Form id="login-form">
-            <Input type="email" name="email" id="inputEmail" placeholder="Email Address" required/>
-            <Input type="password" name="password" id="inputPassword" placeholder="Password" required/>
-            <Button color="primary" block>Sign In</Button>
+            <Input type="email" name="email" id="inputEmail" placeholder="Email Address" required onChange={this.handleChange}/>
+            <FormGroup>
+            <Input type="password" name="password" id="inputPassword" placeholder="Password" required onChange={this.handleChange} invalid={this.state.loginError}/>
+            <FormFeedback>{this.state.loginError}</FormFeedback>
+            </FormGroup>
+            <Button color="primary" block onClick={this.handleLogin}>Sign In</Button>
             </Form>
           </CardBody>
         </Card>
@@ -34,4 +83,4 @@ class Login extends Component {
   }
 }
 
-export default Login;
+export default  connect(mapStateToProps,{ loginUser })(Login);
