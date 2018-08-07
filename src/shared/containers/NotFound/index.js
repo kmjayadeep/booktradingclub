d9f43629f563e850935909ce43e912fac85008db
Jayadeep KM <kmjayadeep@gmail.com>
2018-08-07 09:11:39 +0530

    404 redesign

--- src/shared/containers/NotFound/index.js
+++ src/shared/containers/NotFound/index.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'reactstrap';
+import { Container, Row, Col, Button } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+import "./NotFound.css";
 
 class NotFound extends Component {
   render() {
     return (
-      <Jumbotron className="text-center">
-        <h1 className="display-3">Oops!</h1>
-        <p className="lead">The Page you are looking for is not found</p>
-      </Jumbotron>
+      <Container className="pad-top">
+        <Row>
+          <Col className="text-center">
+            <span className="display-1 d-block">404</span>
+            <div className="mb-4 lead">The page you are looking for was not found.</div>
+            <Button><Link to="/">Back to Home</Link></Button>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
